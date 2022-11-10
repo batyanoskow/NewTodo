27bda81c9cc54606b3939c7fc0d76700c0ef19b7
Batyanoskow <Lyubchenko2910@gmail.com>
2022-11-10 20:22:39 +0200

    app init

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,48 @@
-import { createStore } from 'vuex'
+import { createStore  } from 'vuex'
 
-export default createStore({
+export default  createStore({
   state: {
-  },
-  getters: {
+    todoList :  JSON.parse(localStorage.getItem("VUE-Tasks")) || [],  
   },
   mutations: {
+    addTodo(state , text){
+      if(text != ""){
+        state.todoList.push({
+          title : text,
+          done : false , 
+          deleted : false
+        })
+        localStorage.setItem("VUE-Tasks" , JSON.stringify(state.todoList))
+     
+      }
+    },
+    // saveTodos(state , todos){
+    //   todos = localStorage.setItem("VUE-Tasks" , state.todoList)
+    //   state.todoList = todos
+    // },
+    deleteTodo(state , index){
+      state.todoList.splice(index , 1)
+      localStorage.setItem("VUE-Tasks" , JSON.stringify(state.todoList))
+     
+    },
+    doneTodo(state , index){
+      state.todoList.splice(index , 1)
+      localStorage.setItem("VUE-Tasks" , JSON.stringify(state.todoList))
+     
+    },
+
   },
+
   actions: {
+   
   },
+  getters: {
+   
+  },
+ 
+ 
   modules: {
+
   }
+  
 })
